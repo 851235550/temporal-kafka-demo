3a124b18272290b1a89e6e1483397e5cb7797fa5
wyl <yulu.w@qq.com>
2024-08-01 00:05:39 +0800

    feat: Generate 200 children workflow for producer

--- producer/work.go
+++ producer/work.go
@@ -11,8 +11,13 @@ import (
 
 func StartWorker(c client.Client) {
 	w := worker.New(c, "producer-task-queue", worker.Options{})
-	w.RegisterWorkflow(CronProducerWorkflow)
+	w.RegisterWorkflow(CronParentProducerWorkflow)
+	w.RegisterWorkflow(ChildWorkflow)
 	w.RegisterActivity(ProduceMsg)
+
+	childWorker := worker.New(c, "child-task-queue", worker.Options{})
+	childWorker.RegisterWorkflow(ChildWorkflow)
+	childWorker.RegisterActivity(ProduceMsg)
 
 	go func() {
 		err := w.Run(worker.InterruptCh())
@@ -21,16 +26,50 @@ func StartWorker(c client.Client) {
 		}
 	}()
 
+	go func() {
+		err := childWorker.Run(worker.InterruptCh())
+		if err != nil {
+			log.Fatalf("Unable to start child worker: %v", err)
+		}
+	}()
+
 	// Start a workflow execution
 	we, err := c.ExecuteWorkflow(context.Background(), client.StartWorkflowOptions{
 		ID:                       "kafka-produce-workflow",
 		TaskQueue:                "producer-task-queue",
 		WorkflowExecutionTimeout: time.Hour,
-		CronSchedule:             "*/1 * * * *",
-	}, CronProducerWorkflow)
+		CronSchedule:             "*/1 * * * *", // Ensure this schedule is correct
+	}, CronParentProducerWorkflow)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to start workflow: %v", err)
 	}
 
 	log.Printf("Started workflow with ID: %s and RunID: %s\n", we.GetID(), we.GetRunID())
 }
+
+// func StartWorker(c client.Client) {
+// 	w := worker.New(c, "producer-task-queue", worker.Options{})
+// 	w.RegisterWorkflow(CronParentProducerWorkflow)
+// 	w.RegisterWorkflow(ChildWorkflow)
+// 	w.RegisterActivity(ProduceMsg)
+
+// 	go func() {
+// 		err := w.Run(worker.InterruptCh())
+// 		if err != nil {
+// 			log.Fatalf("Unable to start producer worker: %v", err)
+// 		}
+// 	}()
+
+// 	// Start a workflow execution
+// 	we, err := c.ExecuteWorkflow(context.Background(), client.StartWorkflowOptions{
+// 		ID:                       "kafka-produce-workflow",
+// 		TaskQueue:                "producer-task-queue",
+// 		WorkflowExecutionTimeout: time.Hour,
+// 		CronSchedule:             "*/1 * * * *",
+// 	}, CronParentProducerWorkflow)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	log.Printf("Started workflow with ID: %s and RunID: %s\n", we.GetID(), we.GetRunID())
+// }
--- producer/workflow.go
+++ producer/workflow.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"strconv"
 	"time"
 
 	"github.com/segmentio/kafka-go"
@@ -17,28 +16,25 @@ const (
 )
 
 // ProduceMsg is the main workflow for producing messages to Kafka
-func ProduceMsg(ctx context.Context) error {
+func ProduceMsg(_ context.Context, msg string) error {
+	// fmt.Println("Producing message:", msg)
 	// Kafka writer configuration
 	writer := &kafka.Writer{
 		Addr:     kafka.TCP(kafkaBrokerURL),
 		Topic:    kafkaTopic,
 		Balancer: &kafka.LeastBytes{},
 	}
-	log.Println("Start produce msg...")
-	// Send 200 messages to Kafka
-	for i := 0; i < 200; i++ {
-		log.Println("Start produce msg-" + strconv.Itoa(i))
-		message := kafka.Message{
-			Key:   []byte(fmt.Sprintf("Key-%d", i)),
-			Value: []byte(fmt.Sprintf("Message-%d", i)),
-		}
-		err := writer.WriteMessages(context.Background(), message)
-		if err != nil {
-			log.Printf("Error writing message to Kafka: %v", err)
-			return err
-		}
-		log.Println("End produce msg-" + strconv.Itoa(i))
+
+	log.Printf("Start produce msg: %s\n", msg)
+	message := kafka.Message{
+		Value: []byte(msg),
 	}
+	err := writer.WriteMessages(context.Background(), message)
+	if err != nil {
+		log.Printf("Error writing message to Kafka: %v", err)
+		return err
+	}
+	log.Printf("End produce msg: %s\n", msg)
 
 	// Close the writer
 	if err := writer.Close(); err != nil {
@@ -49,14 +45,58 @@ func ProduceMsg(ctx context.Context) error {
 	return nil
 }
 
-// CronProducerWorkflow is the cron workflow that triggers the producer activity every minute
-func CronProducerWorkflow(ctx workflow.Context) error {
+func CronParentProducerWorkflow(ctx workflow.Context) error {
+	logger := workflow.GetLogger(ctx)
+
+	childWorkflowResults := make([]workflow.Future, 0, 200)
+	// Start 200 child workflows asynchronously
+	for i := 0; i < 200; i++ {
+		// Define child workflow options
+		childWorkflowOptions := workflow.ChildWorkflowOptions{
+			WorkflowID: fmt.Sprintf("child-%d", i),
+			TaskQueue:  "child-task-queue",
+		}
+		childCtx := workflow.WithChildOptions(ctx, childWorkflowOptions)
+		future := workflow.ExecuteChildWorkflow(childCtx, ChildWorkflow, fmt.Sprintf("msg-%d", i))
+		childWorkflowResults = append(childWorkflowResults, future)
+	}
+
+	// Wait for all child workflows to complete
+	for i, future := range childWorkflowResults {
+		err := future.Get(ctx, nil)
+		if err != nil {
+			logger.Error(fmt.Sprintf("Failed to execute child workflow %d: %v", i, err))
+			return err
+		}
+	}
+
+	return nil
+}
+
+func ChildWorkflow(ctx workflow.Context, msg string) error {
 	// Define activity options
 	ao := workflow.ActivityOptions{
-		StartToCloseTimeout: time.Second * 20,
+		StartToCloseTimeout: time.Second * 30,
 	}
 	ctx = workflow.WithActivityOptions(ctx, ao)
 
 	// Execute the activity
-	return workflow.ExecuteActivity(ctx, ProduceMsg).Get(ctx, nil)
+	err := workflow.ExecuteActivity(ctx, ProduceMsg, msg).Get(ctx, nil)
+	if err != nil {
+		logger := workflow.GetLogger(ctx)
+		logger.Error("Failed to execute activity ProduceMsg", "Error", err)
+	}
+	return err
 }
+
+// // CronProducerWorkflow is the cron workflow that triggers the producer activity every minute
+// func CronProducerWorkflow(ctx workflow.Context) error {
+// 	// Define activity options
+// 	ao := workflow.ActivityOptions{
+// 		StartToCloseTimeout: time.Second * 20,
+// 	}
+// 	ctx = workflow.WithActivityOptions(ctx, ao)
+
+// 	// Execute the activity
+// 	return workflow.ExecuteActivity(ctx, ProduceMsg).Get(ctx, nil)
+// }
